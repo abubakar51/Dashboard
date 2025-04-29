1c24f750e7afe5bbe3179104564c723e9dc89ea5
Muhammad Abu Bakar <145241625+abubakar51@users.noreply.github.com>
2025-04-29 09:26:31 +0600

    Update appScript.js

--- appScript.js
+++ appScript.js
@@ -1,14 +1,3 @@
-// const defaultURLs = [
-//   'https://www.youtube.com/watch?v=iEpJwprxDdk',
-//   'https://tradingeconomics.com/commodity/cotton',
-//   'https://futures.tradingcharts.com/futures/quotes/ct.html',
-//   'https://www.google.com/finance/quote/USD-BDT?sa=X&ved=2ahUKEwjnvJXGneOMAxUqxTgGHZ4SKtkQmY0JegQIChAv'
-// ];
-
-// script.js file to be linked in index.html
-
-
-
 
 let activeWebview = null;
 const zoomBackup = new Map(); // <-- Zoom backup per webview
@@ -375,42 +364,3 @@ document.getElementById('reloadBtn').addEventListener('click', () => {
   reloadTabs();
 });
 
-
-
-
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    // Get the body element
-    const body = document.body;
-    
-    // Clear all existing content in the body
-    body.innerHTML = '';
-  tabContainer.innerHTML = '';
-    
-    // Create a new div element
-    const div = document.createElement('div');
-    
-    // Set the div's styles
-    div.style.position = 'absolute';
-    div.style.width = '100%';
-    div.style.height = '100%';
-    div.style.display = 'flex';
-    div.style.justifyContent = 'center';
-    div.style.alignItems = 'center';
-    div.style.backgroundColor = '#fff'; // white background
-    
-    // Create a heading element
-    const heading = document.createElement('h1');
-    heading.textContent = 'I Love You';
-    heading.style.fontSize = '5rem';
-    heading.style.color = '#ff0000'; // red color
-    heading.style.textAlign = 'center';
-    
-    // Append the heading to the div
-    div.appendChild(heading);
-    
-    // Append the div to the body
-    tabContainer.appendChild(div);
-});
